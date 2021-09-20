c6bbccdeac135766cdee94cbf40f00063390b8c3
stackpredict <webmakercreativo@gmail.com>
2021-09-20 17:24:31 -0500

    agregando desafio1

---
+++ app.js
@@ -0,0 +1,6 @@
+let nombreUsuario = prompt("Introduce tu nombre");
+alert("Bienvenido" + " " + nombreUsuario);   //Muestra el nombre de ususario introducido
+let num1 = parseInt(prompt("Ingresa un número del 1 al 10"));
+let num2 = parseInt(prompt("Ingresa un número del 11 al 100"));
+let suma = num1  + num2;
+alert(suma);
